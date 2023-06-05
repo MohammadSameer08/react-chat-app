9fa3a69d20aa59fc53ad16fb0e01d69132236f3d
Md Sameer <ms149132@gmail.com>
2023-06-06 00:19:29 +0530

    fix: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import {ChatEngine} from "react-chat-engine"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ChatEngine
+      height = "100vh"
+      projectID = "2a74737c-1aa1-4d43-aa42-300fad12d43a"
+      userName = "sameer"
+      userSecret = "aftab" 
+    />
     </div>
   );
 }
